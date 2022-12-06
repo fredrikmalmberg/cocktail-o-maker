357e8f41219b1b370d7670fd17bd1fa9da2528ac
”tamdjidi” <tamdjidi@kth.se>
2022-12-06 11:37:49 +0100

    no progress

--- src/components/views/detailsView.vue
+++ src/components/views/detailsView.vue
@@ -1,5 +1,16 @@
-<template>
+<!-- What we want: - Button to add to favorites, if in favorites- disable button-->
+<!-- Image of dish-->
+<!-- Ingredients, How to make, description? -->
 
+<template>
+  <v-app>
+    <v-main>
+      <v-btn
+          elevation="2"
+          raised
+      > Add to favorites</v-btn>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
