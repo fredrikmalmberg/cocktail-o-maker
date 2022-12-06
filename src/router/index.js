f801ff43769c8929cd1ff6feb1c99431eea7c40a
Fredrik Malmberg <fmalmb@kth.se>
2022-12-06 16:07:27 +0100

    Router update for the User Home

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import search from '../components/presenters/searchPresenter.vue'
 import HomeView from '../components/presenters/HomePagePresenter.vue'
 import searchAPITest from '../components/presenters/searchAPIPresenter.vue'
 import detailsAPITest from '../components/presenters/detailsAPIPresenter.vue'
+import userComponent from '../components/presenters/userPresenter.vue'
+
 
 const routes = [
   {
@@ -10,20 +12,21 @@ const routes = [
     name: 'home',
     component: HomeView
   },
-
   {
     path: '/search/:searchstring',
     name: 'search',
     component: searchAPITest
   },
-
-
   { 
     path: '/drinkdetails/:id', 
     name: 'drinkDetails', 
     component: detailsAPITest 
   },
-  
+  { 
+    path: '/user/:name', 
+    name: 'userHome', 
+    component: userComponent 
+  },
   {
     path: '/research',
     name: 'main page',
