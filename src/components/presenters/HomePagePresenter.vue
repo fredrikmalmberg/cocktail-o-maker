3383d8b4cd7b4f39faa11ff533f1ade719848bb5
dilvan <dilvan@kth.se>
2022-12-08 20:38:56 +0100

    login basic

--- src/components/presenters/HomePagePresenter.vue
+++ src/components/presenters/HomePagePresenter.vue
@@ -1,55 +1,126 @@
 <!-- This file contains the HomePage. Currently contains two buttons.-->
-
-
 <template>
-  <div class="text-center">
-    <v-sheet
-        class="px-7 pt-7 pb-4 mx-auto text-center d-inline-block"
-        color="light-grey darken-3"
-        dark
-    >
-      <div class="grey--text text--lighten-1 text-body-2 mb-4">
-        To continue to our page with cocktails, are you over 20 years old?
-      </div>
+  <div class="auth-page">
+    <h1>Login</h1>
+    <form @submit.prevent="login">
+      <label>
+        Email:
+        <input type="email" v-model="email" />
+      </label>
+      <label>
+        Password:
+        <input type="password" v-model="password" />
+      </label>
+      <v-button type="submit">Login</v-button>
+      <p v-if="error" class="error">{{ error }}</p>
+    </form>
 
-      <v-btn
-          :disabled="loading"
-          class="ma-1"
-          color="grey"
-          plain
-          @click="userStore.confirmedUnderTwenty()"
-      >
-        <router-link to="/research">Yes</router-link>
-      </v-btn>
+ 
 
-      <v-btn
-          :loading="loading"
-          class="ma-1"
-          color="error"
-          plain
-          @click="remove"
-      >
-        <router-link to="/">No</router-link>
-      </v-btn>
-    </v-sheet>
+    <h1>Sign up</h1>
+    <v-form @submit.prevent="signup">
+      <label>
+        Name:
+        <input type="text" v-model="name" />
+      </label>
+      <label>
+        Email:
+        <input type="email" v-model="email" />
+      </label>
+      <label>
+        Password:
+        <input type="password" v-model="password" />
+      </label>
+      <v-button type="submit">Sign up</v-button>
+      <p v-if="error" class="error">{{ error }}</p>
+    </v-form>
   </div>
 </template>
 
+
 <script>
-import {useUserStore} from '../../stores/UserStore'
+import axios from 'axios';
 
 export default {
-  setup() {
-    const userStore = useUserStore();
-    return {userStore};
+  data() {
+    return {
+      name: '',
+      email: '',
+      password: '',
+      error: '',
+    };
   },
-  data: () => ({}),
-}
+  methods: {
+    async login() {
+      try {
+        await axios.post('/api/login', {
+          email: this.email,
+          password: this.password,
+        });
+        // Handle successful login
+      } catch (error) {
+        this.error = 'Failed to login: ' + error.response.data.message;
+      }
+    },
+    async signup() {
+      try {
+        await axios.post('/api/signup', {
+          name: this.name,
+          email: this.email,
+          password: this.password,
+        });
+        // Automatically log the user in with their new account
+        this.login();
+      } catch (error) {
+        this.error = 'Failed to sign up: ' + error.response.data.message;
+      }
+  },
+  },
+};
 </script>
 
-<style scoped>
-div {
-  padding-top: 50px;
+<style>
+.auth-page {
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 30px;
+  border: 1px solid #ccc;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  margin-top: 0;
+}
 
+.auth-page h1 {
+  margin-bottom: 30px;
+}
+
+.auth-page form {
+  width: 50%;
+}
+
+.auth-page label {
+  display: block;
+  margin-bottom: 10px;
+}
+
+.auth-page input {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+
+.auth-page button {
+  padding: 10px 20px;
+  border: none;
+  background: #333;
+  color: #fff;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.auth-page .error {
+  color: red;
 }
 </style>
