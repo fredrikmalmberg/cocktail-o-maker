21d32af4dfc8e31a016f5b77eb692a686ccc58a4
dilvan <dilvan@kth.se>
2022-12-11 17:23:06 +0100

    users now has to confirm their age before registering

--- src/components/presenters/registerComponent.vue
+++ src/components/presenters/registerComponent.vue
@@ -1,42 +1,62 @@
 <template>
   <v-container fill-height>
     <v-row no-gutters>
-      <v-col cols="12" class="title mb-5 text-center">
-        Register
-      </v-col>
+      <v-col cols="12" class="title mb-5 text-center"> Register </v-col>
       <v-col cols="12">
         <v-text-field
-            label="Display name"
-            placeholder="Display name"
-            outlined
-            hide-details
-            class="mb-2"
-            v-model="displayNameModel"
+          label="Display name"
+          placeholder="Display name"
+          outlined
+          hide-details
+          class="mb-2"
+          v-model="displayNameModel"
         ></v-text-field>
       </v-col>
       <v-col cols="12">
         <v-text-field
-            label="Email"
-            placeholder="Email"
-            outlined hide-details
-            class="mb-2"
-            v-model="emailModel"
+          label="Email"
+          placeholder="Email"
+          outlined
+          hide-details
+          class="mb-2"
+          v-model="emailModel"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          label="Password"
+          placeholder="Password"
+          outlined
+          hide-details
+          class="mb-5"
+          type="password"
+          v-model="passwordModel"
+        ></v-text-field>
+      </v-col>
 
-        ></v-text-field>
-      </v-col>
+
       <v-col cols="12">
-        <v-text-field
-            label="Password"
-            placeholder="Password"
-            outlined hide-details
-            class="mb-5"
-            type="password"
-            v-model="passwordModel"
-        ></v-text-field>
-      </v-col>
-      <v-col cols="12">
-        <v-btn block large depressed color="orange darken--2" dark @click="registerClicked()">Register</v-btn>
-      </v-col>
+  <input
+    type="checkbox"
+    v-model="acceptTerms"
+  >
+  I hereby confirm that I am over 18 years old
+  <br>
+  <v-btn
+    block
+    large
+    depressed
+    color="orange darken--2"
+    dark
+    v-if="acceptTerms"
+    @click="registerClicked()"
+  >
+    Register
+  </v-btn>
+</v-col>
+
+
+
     </v-row>
   </v-container>
 </template>
@@ -49,25 +69,31 @@ import {
   //updateProfile,
   //onAuthStateChanged,
   //signOut,
-} from 'firebase/auth'
+} from "firebase/auth";
 
 export default {
   data() {
     return {
-      emailModel: '',
-      passwordModel: '',
-      displayNameModel: ''
-    }
+      emailModel: "",
+      passwordModel: "",
+      displayNameModel: "",
+      acceptTerms: false,
+    };
   },
   methods: {
     registerClicked() {
-      const auth = getAuth()
+      const auth = getAuth();
+      if(this.acceptTerms){
       createUserWithEmailAndPassword(auth, this.emailModel, this.passwordModel)
-          .then(() => {
-            this.$router.push({name: 'login'})
-          }).catch(error => {
-        console.log(error)
-      })
+        .then(() => {
+          this.$router.push({ name: "login" });
+        })
+        .catch((error) => {
+          console.log(error);
+        });}
+        else{
+          alert("Unfortunately you cannot become a member if you're not over 18.")
+        }
       //   this.register({email: this.emailModel, password: this.passwordModel, displayName: this.displayNameModel})
       //       .then(() => {
       //         this.$router.push({ name: 'Login' })
@@ -75,11 +101,8 @@ export default {
       //         console.log(error)
       //       })
     },
-
-  }
-}
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
