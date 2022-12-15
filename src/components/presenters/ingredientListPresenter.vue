e5a9476dca89337a30daf70e2656dfaf34b73427
Fredrik Malmberg <fmalmb@kth.se>
2022-12-15 19:07:45 +0100

    Added functionality to filter ingredients

--- src/components/presenters/ingredientListPresenter.vue
+++ src/components/presenters/ingredientListPresenter.vue
@@ -1,25 +1,55 @@
 <template>
-<div v-if="resultPromiseState.data" >
-  <span v-for="(i, item) in resultPromiseState.data.drinks" v-bind:key="item">
-    <ingredientSearchPresenter :ingredientName="i.strIngredient1"/>
-  </span>
-
-</div>
+  <div v-if="resultPromiseState.data">
+    <v-text-field  label="Search ingredients.." variant="solo"
+            clearable v-model="searchTerm"  />
+    <span v-for="(i, item) in allIngredientNames" v-bind:key="item">
+      <ingredientSearchPresenter :ingredientName="i" />
+    </span>
+  </div>
 </template>
 
 <script>
-import {getIngredientList} from '../../cocktailDBIntegration';
-import ingredientSearchPresenter from './ingredientSearchPresenter.vue'
+import { getIngredientList } from "../../cocktailDBIntegration";
+import ingredientSearchPresenter from "./ingredientSearchPresenter.vue";
 export default {
   components: {
     ingredientSearchPresenter,
   },
-    mounted() {
+  mounted() {
     this.resultPromiseState = getIngredientList();
   },
-    data() {
-    return { resultPromiseState: { data: null, error: null} };
+  data() {
+    return {
+      searchTerm: "",
+      resultPromiseState: { data: null, error: null },
+    };
   },
-
-}
+  computed: {
+    allIngredientNames() {
+      let arr = [];
+      if (this.resultPromiseState.data) {
+        for (let n = 0; n < this.resultPromiseState.data.drinks.length; n++) {
+          if (!this.searchTerm || this.searchTerm === "") {
+            arr = [
+              ...arr,
+              this.resultPromiseState.data.drinks[n].strIngredient1,
+            ];
+          } else {
+            if (
+              this.resultPromiseState.data.drinks[n].strIngredient1
+                .toLowerCase()
+                .includes(this.searchTerm.toLowerCase())
+            ) {
+              arr = [
+                ...arr,
+                this.resultPromiseState.data.drinks[n].strIngredient1,
+              ];
+            }
+          }
+        }
+      }
+      return arr;
+    },
+  },
+};
 </script>
