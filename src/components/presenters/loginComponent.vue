f0a718fd9e4b4ec3500cc48ab99676b413be3243
”tamdjidi” <tamdjidi@kth.se>
2022-12-16 13:07:14 +0100

    forgot to reformat code...

--- src/components/presenters/loginComponent.vue
+++ src/components/presenters/loginComponent.vue
@@ -2,40 +2,40 @@
   <v-container fill-height @keydown.enter.prevent="submit()">
     <v-responsive class="d-flex align-center text-center fill-height">
       <v-form
-        ref="form"
-        v-model="valid"
-        lazy-validation
+          ref="form"
+          v-model="valid"
+          lazy-validation
       >
-       
+
         <v-text-field
-          v-model="emailModel"
-          :rules="emailRules"
-          label="E-mail"
-          required
-          class="mb-2"
+            v-model="emailModel"
+            :rules="emailRules"
+            label="E-mail"
+            required
+            class="mb-2"
         ></v-text-field>
 
         <v-text-field
-          v-model="passwordModel"
-          :rules="passwordRules"
-          label="password"
-          type="password"
-          required
-          class="mb-2"
+            v-model="passwordModel"
+            :rules="passwordRules"
+            label="password"
+            type="password"
+            required
+            class="mb-2"
         ></v-text-field>
-    
+
 
         <v-btn
-          color="orange"
-          class="mt-7"
-          @click="submit"
-          variant="flat"
-          block
+            color="orange"
+            class="mt-7"
+            @click="submit"
+            variant="flat"
+            block
         >
           Log in
         </v-btn>
         <div v-if="loginError" class="mt-3 text-red">
-          {{loginError}}
+          {{ loginError }}
         </div>
       </v-form>
     </v-responsive>
@@ -59,38 +59,38 @@ export default {
     return {
       valid: true,
       loginError: "",
-    emailModel: '',
-    emailRules: [
-      v => !!v || 'E-mail is required',
-      v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-    ],
-    passwordModel: '',
-    passwordRules: [
-      v => !!v || 'password is required',
-      v => (v && v.length > 6) || 'password must be longer than 6 characters',
-    ],
+      emailModel: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      passwordModel: '',
+      passwordRules: [
+        v => !!v || 'password is required',
+        v => (v && v.length > 6) || 'password must be longer than 6 characters',
+      ],
 
       userStore: useUserStore()
     }
   },
   methods: {
     async submit() {
-      const { valid } = await this.$refs.form.validate()
-      if (valid){
+      const {valid} = await this.$refs.form.validate()
+      if (valid) {
         const auth = getAuth()
-      signInWithEmailAndPassword(auth, this.emailModel, this.passwordModel)
-          .then(response => {
-            /* eslint-disable */
-            console.log(response)
-            //this.userStore.setCurrentUser(response);
-            this.$router.push({name: 'userHome'});
-          }).catch(error => {
-            this.loginError = "User not found"
-        console.log(error)
-      })
-        
+        signInWithEmailAndPassword(auth, this.emailModel, this.passwordModel)
+            .then(response => {
+              /* eslint-disable */
+              console.log(response)
+              //this.userStore.setCurrentUser(response);
+              this.$router.push({name: 'userHome'});
+            }).catch(error => {
+          this.loginError = "User not found"
+          console.log(error)
+        })
+
       }
-      
+
     },
 
   }
--- src/components/presenters/registerComponent.vue
+++ src/components/presenters/registerComponent.vue
@@ -2,50 +2,50 @@
   <v-container fill-height @keydown.enter.prevent="submit()">
     <v-responsive class="d-flex align-center text-center fill-height">
       <v-form
-        ref="form"
-        v-model="valid"
-        lazy-validation
+          ref="form"
+          v-model="valid"
+          lazy-validation
       >
         <v-text-field
-          v-model="displayNameModel"
-          :counter="10"
-          :rules="displayNameRules"
-          label="Name"
-          class="mb-2"
-          required
+            v-model="displayNameModel"
+            :counter="10"
+            :rules="displayNameRules"
+            label="Name"
+            class="mb-2"
+            required
         ></v-text-field>
 
         <v-text-field
-          v-model="emailModel"
-          :rules="emailRules"
-          label="E-mail"
-          required
-          class="mb-2"
+            v-model="emailModel"
+            :rules="emailRules"
+            label="E-mail"
+            required
+            class="mb-2"
         ></v-text-field>
 
         <v-text-field
-          v-model="passwordModel"
-          :rules="passwordRules"
-          label="password"
-          type="password"
-          required
-          class="mb-2"
+            v-model="passwordModel"
+            :rules="passwordRules"
+            label="password"
+            type="password"
+            required
+            class="mb-2"
         ></v-text-field>
 
         <v-checkbox
 
-          v-model="acceptTerms"
-          :rules="[v => !!v || 'You have to be over 18 to use our website!']"
-          label="Are you over 18 years?"
-          required
+            v-model="acceptTerms"
+            :rules="[v => !!v || 'You have to be over 18 to use our website!']"
+            label="Are you over 18 years?"
+            required
         ></v-checkbox>
 
         <v-btn
-          color="orange"
-          class="mt-7"
-          @click="submit"
-          variant="flat"
-          block
+            color="orange"
+            class="mt-7"
+            @click="submit"
+            variant="flat"
+            block
         >
           Submit
         </v-btn>
@@ -68,34 +68,34 @@ import {useUserStore} from '../../stores/UserStore';
 export default {
   data() {
     return {
-      
+
       userStore: useUserStore(),
       valid: true,
       acceptTerms: false,
-    displayNameModel: '',
-    displayNameRules: [
-      v => !!v || 'Name is required',
-      v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-    ],
-    emailModel: '',
-    emailRules: [
-      v => !!v || 'E-mail is required',
-      v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-    ],
-    passwordModel: '',
-    passwordRules: [
-      v => !!v || 'password is required',
-      v => (v && v.length > 6) || 'password must be longer than 6 characters',
-    ],
+      displayNameModel: '',
+      displayNameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+      ],
+      emailModel: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      passwordModel: '',
+      passwordRules: [
+        v => !!v || 'password is required',
+        v => (v && v.length > 6) || 'password must be longer than 6 characters',
+      ],
     };
   },
   methods: {
     /* eslint-disable */
     async submit() {
-      const { valid } = await this.$refs.form.validate()
-      if (valid){
+      const {valid} = await this.$refs.form.validate()
+      if (valid) {
         const auth = getAuth();
-      createUserWithEmailAndPassword(auth, this.emailModel, this.passwordModel)
+        createUserWithEmailAndPassword(auth, this.emailModel, this.passwordModel)
             .then(() => {
               this.userStore.username = this.displayNameModel
               this.$router.push({name: "login"});
@@ -105,15 +105,15 @@ export default {
             });
 
       }
-      
-      
+
+
     },
   },
 };
 </script>
 
 <style scoped>
-.v-input__details{
+.v-input__details {
   text-align: left;
   padding-left: 15px;
 }</style>
